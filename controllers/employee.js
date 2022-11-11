f966b617f013662cee4a7d4a3ca782cc708e79be
DeekshaChelamalla25 <112186227+DeekshaChelamalla25@users.noreply.github.com>
2022-11-10 21:22:52 -0600

    screenshot 1

--- controllers/employee.js
+++ controllers/employee.js
@@ -16,6 +16,13 @@ exports.employee_list = async function(req, res) {
 // for a specific employee. 
 exports.employee_detail = async function(req, res) { 
     console.log("detail" + req.params.id)
+    try { 
+        result = await Costume.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
 }; 
  
 // Handle employee create on POST. 
